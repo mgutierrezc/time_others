47699e14be928b5775c2ebf35828104099054e6a
Sergio2405 <67673799+Sergio2405@users.noreply.github.com>
2022-02-25 10:51:26 -0500

    format per block html

--- time_others/static/js/graph_es.js
+++ time_others/static/js/graph_es.js
@@ -417,9 +417,9 @@ var vm = new Vue({
 
                     if (['independent', 'single', 'negative', 'single_fixedsquare', 'single_fixedcircle'].indexOf(self.mode) !== -1) {
                         if (index === 0) {
-                            text = 'Tú (A: ' + self.selected[index].x + ', B: ' + self.selected[index].y + ')'
+                            text = 'Tú (Hoy: ' + self.selected[index].x + ', Mañana: ' + self.selected[index].y + ')'
                         }else{
-                            text = 'Pareja (A: ' + self.selected[index].x + ', B: ' + self.selected[index].y + ')'
+                            text = 'Pareja (Hoy: ' + self.selected[index].x + ', Mañana: ' + self.selected[index].y + ')'
                         }
                     }else if (self.mode === 'single_given') {
                         text = 'Tú: ' + self.selected[index].x + ', Pareja: ' + self.selected[index].y
@@ -460,9 +460,9 @@ var vm = new Vue({
 
                     if (['independent', 'single', 'negative', 'single_fixedsquare', 'single_fixedcircle'].indexOf(self.mode) !== -1) {
                         if (index == 0) {
-                            text = 'Tú (A: ' + xValue.toFixed(self.precision) + ', B: ' + yValue.toFixed(self.precision) + ')'
+                            text = 'Tú (Hoy: ' + xValue.toFixed(self.precision) + ', Mañana: ' + yValue.toFixed(self.precision) + ')'
                         }else{
-                            text = 'Pareja (A: ' + xValue.toFixed(self.precision) + ', B: ' + yValue.toFixed(self.precision) + ')'
+                            text = 'Pareja (Hoy: ' + xValue.toFixed(self.precision) + ', Mañana: ' + yValue.toFixed(self.precision) + ')'
                         }
                     }else if (self.mode === 'single_given') {
                         text = 'Tu: ' + xValue.toFixed(self.precision) + ', Pareja: ' + yValue.toFixed(self.precision)
@@ -509,9 +509,9 @@ var vm = new Vue({
                             var otherText = ''
 
                             if (otherIndex == 0) {
-                                otherText = 'Tú (A: ' + otherXValue.toFixed(self.precision) + ', B: ' + otherYValue.toFixed(self.precision) + ')'
+                                otherText = 'Tú (Hoy: ' + otherXValue.toFixed(self.precision) + ', Mañana: ' + otherYValue.toFixed(self.precision) + ')'
                             }else{
-                                otherText = 'Pareja (A: ' + otherXValue.toFixed(self.precision) + ', B: ' + otherYValue.toFixed(self.precision) + ')'
+                                otherText = 'Pareja (Hoy: ' + otherXValue.toFixed(self.precision) + ', Mañana: ' + otherYValue.toFixed(self.precision) + ')'
                             }
 
                             if (self.tip && self.tip[otherIndex]) {
